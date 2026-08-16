a4da0c235f9c89557d9ecf0a4eeb286c699dfebe
dhanashrikatkar99 <dhanashrikatkargdsc@gmail.com>
2026-08-16 16:47:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ st.set_page_config(
 # ============================================================
 
 weather = pd.read_csv(
-    r"D:\My_Projects\Python_Projects\Indian Weather Data Analysis\Dataset\Indian_Weather_Consolidated.zip"
+    r"Dataset\Indian_Weather_Consolidated.zip"
 )
 
 
